afe4fa67c29204ca71539db446a778acb68913f1
rgorai <43449685+rgorai@users.noreply.github.com>
2021-12-18 14:54:51 -0500

    fixes

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ if (process.env.NODE_ENV === 'production') {
   console.log('serving prod build')
   app.use(express.static(path.resolve('client', 'build')))
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve('client', 'build', 'index.html'))
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
   })
 } else {
   configRoutes(app)
