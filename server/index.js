b31608c7fdec745d7ad005798c5b48c564f776c8
rgorai <43449685+rgorai@users.noreply.github.com>
2021-12-20 00:04:41 -0500

    fixing axios in prod

--- server/index.js
+++ server/index.js
@@ -1,19 +1,19 @@
 import express from 'express'
 import cors from 'cors'
 import path from 'path'
-import { fileURLToPath } from 'url'
 import dotenv from 'dotenv'
 import configRoutes from './routes/index.js'
 
 dotenv.config()
 
 const PORT = process.env.PORT || 5000
-const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 const app = express()
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+
+configRoutes(app)
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.resolve('client', 'build')))
@@ -21,8 +21,6 @@ if (process.env.NODE_ENV === 'production') {
     res.sendFile(path.resolve('client', 'build', 'index.html'))
   })
 }
-
-configRoutes(app)
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`)
--- server/routes/index.js
+++ server/routes/index.js
@@ -4,9 +4,9 @@ import guestbookRouter from './guestbook.js'
 const configRoutes = (app) => {
   app.use('/api/guestbook', guestbookRouter)
 
-  app.use('*', (req, res) => {
-    res.sendStatus(404)
-  })
+  // app.use('*', (req, res) => {
+  //   res.sendStatus(404)
+  // })
 }
 
 export default configRoutes
