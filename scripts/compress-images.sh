c5e2a919d7e43b605053bcb6fb92ecb660c9fb48
Ron Gorai <ron.gorai2020@gmail.com>
2026-02-02 19:24:23 -0500

    s3 scripts

--- scripts/compress-images.sh
+++ scripts/compress-images.sh
@@ -1,25 +1,28 @@
 #!/bin/bash
 
 # Runs cwebp recursively in the specified directory on all image files
+# Outputs compressed webp files to _compressed/ subdirectory
 
 PARAMS=('-m 6 -q 60 -mt -af -progress')
-
-if [ $# -ne 0 ]; then
-	PARAMS=$@;
-fi
-
-cd $(pwd)
+BASE_DIR="${1:-.local/s3-bucket}"
+COMPRESSED_DIR="$BASE_DIR/_compressed"
 
 shopt -s nullglob nocaseglob extglob
 
 convert () {
   for FILE in $1/*; do
-    if [[ -d $FILE ]]; then
-      convert $FILE
-    elif [[ $FILE == *.@(jpg|jpeg|tif|tiff|png) ]]; then 
-      cwebp $PARAMS "$FILE" -o "${FILE%.*}".webp;
+    if [[ -d $FILE ]] && [[ "$(basename $FILE)" != "_compressed" ]]; then
+      convert "$FILE"
+    elif [[ $FILE == *.@(jpg|jpeg|tif|tiff|png) ]]; then
+      # Get relative path from base dir and create output path under _compressed
+      REL_PATH="${FILE#$BASE_DIR/}"
+      OUT_DIR="$COMPRESSED_DIR/$(dirname "$REL_PATH")"
+      OUT_FILE="$OUT_DIR/$(basename "${FILE%.*}").webp"
+
+      mkdir -p "$OUT_DIR"
+      cwebp $PARAMS "$FILE" -o "$OUT_FILE"
     fi
   done
 }
 
-convert "${1:-.local/s3-bucket}"
+convert "$BASE_DIR"
